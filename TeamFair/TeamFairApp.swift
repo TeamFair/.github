9ef268b54ff56074cc06056a269e5e1b66a2e964
chad0909@naver.com <108044639+chad0909@users.noreply.github.com>
2023-07-01 17:05:34 +0900

    git ignore added

---
+++ TeamFair/TeamFairApp.swift
@@ -0,0 +1,17 @@
+//
+//  TeamFairApp.swift
+//  TeamFair
+//
+//  Created by 077tech on 2023/07/01.
+//
+
+import SwiftUI
+
+@main
+struct TeamFairApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
